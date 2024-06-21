c70be0a7439002d37b6013314946805b4d656338
anambi82 <anambi@tamu.edu>
2024-06-21 14:20:19 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/members")
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json.members || []);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Members</h1>
+      {data.map((member, i) => (
+        <div key={i}>
+          <h3>{member.name || member}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/members')
+def members():
+    return jsonify({"members": ["member1", "member2", "member3"]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
